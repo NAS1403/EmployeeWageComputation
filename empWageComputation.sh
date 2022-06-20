c91aeb8dc2291de98a7bb4ba6669a1f0b454cb75
N Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-06-20 15:21:06 +0530

    [ABHI]Compute daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,17 @@
 echo "Welcome to Employee Wage Computation Program"
 
+Wage_per_hour=20
+Full_day_hour=8
+Daily_wage=0
+
 a=$((RANDOM%2))
 
 if [ $a -eq 0 ]
 then
-	echo "Employee is present"
+	echo -e "Employee is present\n"
+	Daily_wage=$(($Wage_per_hour*$Full_day_hour))
+	echo "Employee Daily wage is " $Daily_wage
 else
 	echo "Employee is absent"
+	echo "Employee Daily wage is " $Daily_wage
 fi
