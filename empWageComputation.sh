f582b1b24568ecdebe6d677d470234a991e975e9
N Abhishek Sagar <abhisheksagar1403@gmail.com>
2022-06-20 16:26:34 +0530

    [ABHI]conditon for wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,28 +6,38 @@ Half_day_hour=4
 Month_work_days=20
 Daily_wage=0
 Monthly_wage=0
+Total_work_hours=0
+Total_work_days=0
 
+
+while [ $Total_work_hours -le 100 ] && [ $Total_work_days -le 20 ]
+do
 a=$((RANDOM%3))
-
 case $a in
 
 	0)
-	echo -e "Employee is present\n"
+	echo -e "\nEmployee is present\n"
 	Daily_wage=$(($Wage_per_hour*$Full_day_hour))
 	Monthly_wage=$(($Daily_wage*$Month_work_days))
 	echo "Employee Daily wage is " $Daily_wage  
 	echo "Employee Monthly wage is " $Monthly_wage
+	Total_work_hours=$(($Total_work_hours+$Full_day_hour))
 	;;
 
 	1)
-	echo -e "Employee working partime\n"
+	echo -e "\nEmployee working partime\n"
 	Daily_wage=$(($Wage_per_hour*$Half_day_hour))
         Monthly_wage=$(($Daily_wage*$Month_work_days))
         echo "Employee Daily wage is " $Daily_wage
 	echo "Employee Monthly wage is " $Monthly_wage
+        Total_work_hours=$(($Total_work_hours+$Half_day_hour))
 	;;
 
 	2)
-	echo "Employee is absent"
+	echo -e "\nEmployee is absent"
 	;;
 esac
+Total_work_days=$(($Total_work_days+1))
+done
+echo $Total_work_hours
+echo $Total_work_days
